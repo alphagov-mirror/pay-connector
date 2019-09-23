168a649041d3356b8742ba533e8dcf075d05a673
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2019-09-23 15:28:35 +0100

    PP-5649 Generalise Card Type and Update Tests
    - Gerenralises the name of CardType from Supported type, updates enum
    validaiton on string conversion and updates tests to add further peace
    of mind re correctness

--- src/main/java/uk/gov/pay/connector/cardtype/model/domain/CardType.java
+++ src/main/java/uk/gov/pay/connector/cardtype/model/domain/CardType.java
@@ -4,11 +4,12 @@ package uk.gov.pay.connector.cardtype.model.domain;
  * Internal entity used to drive the frontend UI based on the
  * strings stored in the table {@code card_types}. This represents
  * which card types are supported by the service, not what card types
- * are used for payment by the paying user
+ * are used for payment by the paying user, which is {@code PayersCardType}
  * <p>
- * This should be used only for driving the UI
+ * This should be used only for instances where the type of the card will either be
+ * Credit or Debit, but there is no CREDIT_OR_DEBIT state as exists in PayersCardType
  */
-public enum SupportedType {
+public enum CardType {
     CREDIT,
     DEBIT
 }
