6a3df3f2609ad94b693eafac9380af08dc059f2a
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2020-02-12 15:58:46 +0000

    PP-6130 Fix Id Errors
    - Fix the ID errors and the errors regarding the timezone and wallet
    type comparisons

--- src/test/java/uk/gov/pay/connector/it/resources/ExpungeResourceIT.java
+++ src/test/java/uk/gov/pay/connector/it/resources/ExpungeResourceIT.java
@@ -18,6 +18,7 @@ import uk.gov.pay.connector.junit.TestContext;
 import uk.gov.pay.connector.util.DatabaseTestHelper;
 
 import java.time.ZonedDateTime;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
@@ -27,6 +28,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
+import static uk.gov.pay.commons.model.ApiResponseDateTimeFormatter.ISO_INSTANT_MILLISECOND_PRECISION;
 import static uk.gov.pay.commons.model.SupportedLanguage.ENGLISH;
 import static uk.gov.pay.connector.junit.DropwizardJUnitRunner.WIREMOCK_PORT;
 
@@ -64,11 +66,12 @@ public class ExpungeResourceIT {
 
     @Test
     public void shouldExpungeCharge() throws JsonProcessingException {
+        var chargedId = ThreadLocalRandom.current().nextLong();
         ledgerStub = new LedgerStub();
         expungeableCharge1 = DatabaseFixtures.withDatabaseTestHelper(databaseTestHelper)
                 .aTestCharge()
-                .withChargeId(1L)
-                .withCreatedDate(ZonedDateTime.now(UTC).minusDays(91))
+                .withChargeId(chargedId)
+                .withCreatedDate(ZonedDateTime.parse(ISO_INSTANT_MILLISECOND_PRECISION.format(ZonedDateTime.now(UTC).minusDays(91))))
                 .withTestAccount(defaultTestAccount)
                 .withExternalChargeId("external_charge_id")
                 .withTransactionId("gateway_transaction_id")
@@ -94,11 +97,12 @@ public class ExpungeResourceIT {
     
     @Test
     public void shouldUpdateTheParityCheckedDateOfNonCriteriaMatchedCharge() throws JsonProcessingException {
+        var chargedId = ThreadLocalRandom.current().nextLong();
         ledgerStub = new LedgerStub();
-        var date = ZonedDateTime.now(UTC).minusDays(91);
+        var date = ZonedDateTime.parse(ISO_INSTANT_MILLISECOND_PRECISION.format(ZonedDateTime.now(UTC).minusDays(91)));
         expungeableCharge1 = DatabaseFixtures.withDatabaseTestHelper(databaseTestHelper)
                 .aTestCharge()
-                .withChargeId(1L)
+                .withChargeId(chargedId)
                 .withCreatedDate(date)
                 .withTestAccount(defaultTestAccount)
                 .withExternalChargeId("external_charge_id")
@@ -119,11 +123,13 @@ public class ExpungeResourceIT {
 
     @Test
     public void shouldNotExpungeChargeThatIsNotOldEnoughToBeExpunged() throws JsonProcessingException {
+        var chargedId = ThreadLocalRandom.current().nextLong();
         ledgerStub = new LedgerStub();
         expungeableCharge1 = DatabaseFixtures.withDatabaseTestHelper(databaseTestHelper)
                 .aTestCharge()
-                .withChargeId(3L)
-                .withCreatedDate(ZonedDateTime.now(UTC).minusDays(89))
+                .withChargeId(chargedId)
+                .withCreatedDate(ZonedDateTime.parse(ISO_INSTANT_MILLISECOND_PRECISION.format(ZonedDateTime.now(UTC)
+                        .minusDays(89))))
                 .withTestAccount(defaultTestAccount)
                 .withExternalChargeId("external_charge_id_2")
                 .withTransactionId("gateway_transaction_id")
@@ -149,12 +155,14 @@ public class ExpungeResourceIT {
 
     @Test
     public void shouldExpungeChargesMeetingCriteriaButNotThoseThatDont() throws JsonProcessingException {
+        var chargedId = ThreadLocalRandom.current().nextLong();
         ledgerStub = new LedgerStub();
 
         expungeableCharge1 = DatabaseFixtures.withDatabaseTestHelper(databaseTestHelper)
                 .aTestCharge()
-                .withChargeId(10L)
-                .withCreatedDate(ZonedDateTime.now(UTC).minusDays(89))
+                .withChargeId(chargedId)
+                .withCreatedDate(ZonedDateTime.parse(ISO_INSTANT_MILLISECOND_PRECISION.format(ZonedDateTime.now(UTC)
+                        .minusDays(89))))
                 .withTestAccount(defaultTestAccount)
                 .withExternalChargeId("external_charge_id_10")
                 .withTransactionId("gateway_transaction_id")
@@ -168,10 +176,13 @@ public class ExpungeResourceIT {
                 .insert();
         ledgerStub.returnLedgerTransaction("external_charge_id_10", expungeableCharge1);
 
+        var chargedId2 = ThreadLocalRandom.current().nextLong();
+
         var nonExpungeableCharge1 = DatabaseFixtures.withDatabaseTestHelper(databaseTestHelper)
                 .aTestCharge()
-                .withChargeId(11L)
-                .withCreatedDate(ZonedDateTime.now(UTC).minusDays(91))
+                .withChargeId(chargedId2)
+                .withCreatedDate(ZonedDateTime.parse(ISO_INSTANT_MILLISECOND_PRECISION.format(ZonedDateTime.now(UTC)
+                        .minusDays(91))))
                 .withTestAccount(defaultTestAccount)
                 .withExternalChargeId("external_charge_id_11")
                 .withTransactionId("gateway_transaction_id")
@@ -185,10 +196,13 @@ public class ExpungeResourceIT {
                 .insert();
         ledgerStub.returnLedgerTransaction("external_charge_id_11", nonExpungeableCharge1);
 
+        var chargedId3 = ThreadLocalRandom.current().nextLong();
+
         var expungeableCharge2 = DatabaseFixtures.withDatabaseTestHelper(databaseTestHelper)
                 .aTestCharge()
-                .withChargeId(12L)
-                .withCreatedDate(ZonedDateTime.now(UTC).minusDays(88))
+                .withChargeId(chargedId3)
+                .withCreatedDate(ZonedDateTime.parse(ISO_INSTANT_MILLISECOND_PRECISION.format(ZonedDateTime.now(UTC)
+                        .minusDays(88))))
                 .withTestAccount(defaultTestAccount)
                 .withExternalChargeId("external_charge_id_12")
                 .withTransactionId("gateway_transaction_id")
@@ -202,10 +216,13 @@ public class ExpungeResourceIT {
                 .insert();
         ledgerStub.returnLedgerTransaction("external_charge_id_12", expungeableCharge2);
 
+        var chargedId4 = ThreadLocalRandom.current().nextLong();
+
         var nonExpungeableCharge2 = DatabaseFixtures.withDatabaseTestHelper(databaseTestHelper)
                 .aTestCharge()
-                .withChargeId(13L)
-                .withCreatedDate(ZonedDateTime.now(UTC).minusDays(92))
+                .withChargeId(chargedId4)
+                .withCreatedDate(ZonedDateTime.parse(ISO_INSTANT_MILLISECOND_PRECISION.format(ZonedDateTime.now(UTC)
+                        .minusDays(92))))
                 .withTestAccount(defaultTestAccount)
                 .withExternalChargeId("external_charge_id_13")
                 .withTransactionId("gateway_transaction_id")
