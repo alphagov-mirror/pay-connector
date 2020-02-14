ef26f02567ab7ed8a9e4d5c375073b1892ffe51e
Krish <krishna.bottla@gmail.com>
2020-02-14 09:30:29 +0000

    PP-6098 Parity checker: various fixes
    - Fixed issues with serialising Ledger transaction (types, constructor on CardDetails & Address)
    - Create `SettlementSummary` for Ledger transaction as ChargeResponse.SettlementSummary doesn't line up with deserialisation
    - Test for LedgerService
    - Paritycheck for metadata -> only checks for existence vs non-existence of metadata on charge and transaction

--- src/main/java/uk/gov/pay/connector/charge/model/ChargeResponse.java
+++ src/main/java/uk/gov/pay/connector/charge/model/ChargeResponse.java
@@ -2,6 +2,7 @@ package uk.gov.pay.connector.charge.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
